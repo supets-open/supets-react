f0b6db654d2a56846b2706a75b5d2944c4fc4afd
B.S.Lee <254608684@qq.com>
2018-03-20 17:05:37 +0800

    热更新接口实现

---
+++ app/src/main/java/com/awesomeproject/activity/LocalActivity.java
@@ -0,0 +1,22 @@
+package com.awesomeproject.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+
+public class LocalActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView name = new TextView(this);
+        setContentView(name);
+
+        String test = getIntent().getStringExtra("test");
+        name.setText(test);
+
+    }
+}
